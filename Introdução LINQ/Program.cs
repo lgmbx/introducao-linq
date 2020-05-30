db47a6704760042eeb22a34769ae9609ec6b3811
lgmbx <gustavo_x@live.com>
2020-05-30 15:39:34 -0300

    Add project files.

---
+++ Introdução LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Introdução_LINQ {
+    class Program {
+        static void Main(string[] args) {
+            //data source
+            int[] num = new int[] { 2, 3, 4, 5 };
+
+            //query expression
+            var result = num.Where(x => x % 2 == 0)
+                            .Select(x => x * 10);
+
+
+            //execute query
+            foreach(int x in result) {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
